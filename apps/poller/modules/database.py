c6cbfc021eb9b308c6e98fd2065d34aa0d0650d7
Scott Crespo <sccrespo@gmail.com>
2026-04-03 14:07:10 -0400

    removed unused modules

--- apps/poller/modules/database.py
+++
@@ -1,9 +0,0 @@
-import os
-import boto3
-
-table_name = os.environ["DYNAMODB_TABLE"]
-
-dynamodb = boto3.resource("dynamodb", endpoint_url=os.getenv("DYNAMODB_ENDPOINT_URL") )
-
-table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
