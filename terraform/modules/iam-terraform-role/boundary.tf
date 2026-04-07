9874838b377a2998c7f8df1f072fd1662bc1cb74
Scott Crespo <sccrespo@gmail.com>
2026-04-07 18:54:32 -0400

    ran terraform fmt

--- terraform/modules/iam-terraform-role/boundary.tf
+++ terraform/modules/iam-terraform-role/boundary.tf
@@ -1,55 +1,55 @@
 resource "aws_iam_policy" "permission_boundary" {
-    name = "terraform-${var.environment}-boundary-for-provisioned-roles"
+  name = "terraform-${var.environment}-boundary-for-provisioned-roles"
 
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Sid      = "AllowEKS"
-                Effect   = "Allow"
-                Action   = "eks:*"
-                Resource = "arn:aws:eks:${var.region}:${var.account_id}:cluster/${local.resource_scope_limit}"
-            },
-            {
-                Sid      = "AllowDynamoDB"
-                Effect   = "Allow"
-                Action   = "dynamodb:*"
-                Resource = "arn:aws:dynamodb:${var.region}:${var.account_id}:table/${local.resource_scope_limit}"
-            },
-            {
-                Sid      = "AllowECRAuthToken"
-                Effect   = "Allow"
-                Action   = "ecr:GetAuthorizationToken"
-                Resource = "*"
-            },
-            {
-                Sid    = "AllowECRRepositoryAccess"
-                Effect = "Allow"
-                Action = [
-                    "ecr:BatchGetImage",
-                    "ecr:GetDownloadUrlForLayer",
-                    "ecr:BatchCheckLayerAvailability"
-                ]
-                Resource = "arn:aws:ecr:${var.region}:${var.account_id}:repository/${local.resource_scope_limit}"
-            },
-            {
-                Sid    = "AllowCloudWatchLogs"
-                Effect = "Allow"
-                Action = [
-                    "logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents",
-                    "logs:DescribeLogGroups",
-                    "logs:DescribeLogStreams"
-                ]
-                Resource = "arn:aws:logs:${var.region}:${var.account_id}:log-group:/${var.project_scope_limit_prefix}/*:*"
-            },
-            {
-                Sid      = "DenyIAM"
-                Effect   = "Deny"
-                Action   = "iam:*"
-                Resource = "*"
-            }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "AllowEKS"
+        Effect   = "Allow"
+        Action   = "eks:*"
+        Resource = "arn:aws:eks:${var.region}:${var.account_id}:cluster/${local.resource_scope_limit}"
+      },
+      {
+        Sid      = "AllowDynamoDB"
+        Effect   = "Allow"
+        Action   = "dynamodb:*"
+        Resource = "arn:aws:dynamodb:${var.region}:${var.account_id}:table/${local.resource_scope_limit}"
+      },
+      {
+        Sid      = "AllowECRAuthToken"
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowECRRepositoryAccess"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchCheckLayerAvailability"
         ]
-    })
+        Resource = "arn:aws:ecr:${var.region}:${var.account_id}:repository/${local.resource_scope_limit}"
+      },
+      {
+        Sid    = "AllowCloudWatchLogs"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "arn:aws:logs:${var.region}:${var.account_id}:log-group:/${var.project_scope_limit_prefix}/*:*"
+      },
+      {
+        Sid      = "DenyIAM"
+        Effect   = "Deny"
+        Action   = "iam:*"
+        Resource = "*"
+      }
+    ]
+  })
 }
